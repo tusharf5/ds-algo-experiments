6a295b26879b15891ca8a77ca2daee2d8f25fd6a
Tushar Sharma <ts17995@gmail.com>
2020-06-28 03:17:31 +0530

    Update bounding-react.ts

--- algo/bounding-react.ts
+++ algo/bounding-react.ts
@@ -1,41 +1,36 @@
-let bounds: Record<"tr" | "tl" | "br" | "bl", [number, number]> = {
+let bounds: Record<'tr' | 'tl' | 'br' | 'bl', [number, number]> = {
   tr: [1, 1],
   tl: [-1, 1],
   br: [1, -1],
   bl: [-1, -1],
 };
 
-function isInside([x, y]: [number, number]):
-  | boolean
-  | "tr"
-  | "tl"
-  | "br"
-  | "bl" {
-  if (x >= bounds["tr"][0] || y >= bounds["tr"][1]) {
-    return "tr";
+function isInside([x, y]: [number, number]): boolean | 'tr' | 'tl' | 'br' | 'bl' {
+  if ((x > 0 && y > 0 && x >= bounds['tr'][0]) || (x > 0 && y > 0 && y >= bounds['tr'][1])) {
+    return 'tr';
   }
 
-  if (x <= bounds["tl"][0] || y >= bounds["tl"][1]) {
-    return "tl";
+  if ((x < 0 && y > 0 && x <= bounds['tl'][0]) || (x > 0 && y > 0 && y >= bounds['tl'][1])) {
+    return 'tl';
   }
 
-  if (x >= bounds["br"][0] || y <= bounds["br"][1]) {
-    return "br";
+  if ((x > 0 && y < 0 && x >= bounds['br'][0]) || (x > 0 && y > 0 && y <= bounds['br'][1])) {
+    return 'br';
   }
 
-  if (x <= bounds["bl"][0] || y <= bounds["bl"][1]) {
-    return "bl";
+  if ((x < 0 && y < 0 && x <= bounds['bl'][0]) || (x > 0 && y > 0 && y <= bounds['bl'][1])) {
+    return 'bl';
   }
 
   return true;
 }
 
 function computeNewBoundingRect(
-  plane: "tr" | "tl" | "br" | "bl",
+  plane: 'tr' | 'tl' | 'br' | 'bl',
   cdn: [number, number],
-  boundingRect: Record<"tr" | "tl" | "br" | "bl", [number, number]>
-): Record<"tr" | "tl" | "br" | "bl", [number, number]> {
-  if (plane === "tl") {
+  boundingRect: Record<'tr' | 'tl' | 'br' | 'bl', [number, number]>
+): Record<'tr' | 'tl' | 'br' | 'bl', [number, number]> {
+  if (plane === 'tl') {
     const newTLX = Math.min(boundingRect.tl[0], cdn[0]);
     const newTLY = Math.max(boundingRect.tl[1], cdn[1]);
     const newTRY = Math.max(boundingRect.tr[1], cdn[1]);
@@ -44,10 +39,10 @@ function computeNewBoundingRect(
       ...boundingRect,
       tl: [newTLX, newTLY],
       tr: [boundingRect.tr[0], newTRY],
-      bl: [newBLX, boundingRect.br[1]],
+      bl: [newBLX, boundingRect.bl[1]],
     };
   }
-  if (plane === "tr") {
+  if (plane === 'tr') {
     const newTRX = Math.max(boundingRect.tr[0], cdn[0]);
     const newTRY = Math.max(boundingRect.tr[1], cdn[1]);
     const newTLY = Math.max(boundingRect.tl[1], cdn[1]);
@@ -59,7 +54,7 @@ function computeNewBoundingRect(
       br: [newBRX, boundingRect.br[1]],
     };
   }
-  if (plane === "br") {
+  if (plane === 'br') {
     const newBRX = Math.max(boundingRect.br[0], cdn[0]);
     const newBRY = Math.min(boundingRect.br[1], cdn[1]);
     const newTRX = Math.max(boundingRect.tr[0], cdn[0]);
@@ -72,7 +67,7 @@ function computeNewBoundingRect(
     };
   }
 
-  if (plane === "bl") {
+  if (plane === 'bl') {
     const newBLX = Math.min(boundingRect.bl[0], cdn[0]);
     const newBLY = Math.min(boundingRect.bl[1], cdn[1]);
     const newTLX = Math.min(boundingRect.tl[0], cdn[0]);
@@ -84,19 +79,20 @@ function computeNewBoundingRect(
       br: [boundingRect.br[0], newBRY],
     };
   }
-}
-type planes = "tr" | "tl" | "br" | "bl";
 
-function addPoint(cdn: [number, number]) {
+  return bounds;
+}
+type planes = 'tr' | 'tl' | 'br' | 'bl';
+
+function addPoint(cdn: [number, number]): boolean {
   const isInsideOrNot = isInside(cdn);
 
-  if (typeof isInsideOrNot === "boolean" && isInsideOrNot) {
+  if (typeof isInsideOrNot === 'boolean' && isInsideOrNot) {
     return true;
   } else {
     const plane = isInsideOrNot as planes;
     const newBound = computeNewBoundingRect(plane, cdn, bounds);
     bounds = newBound;
-
     return true;
   }
 }
@@ -111,7 +107,27 @@ console.log(`
  (${bounds.bl[0]},${bounds.bl[1]})....|.....(${bounds.br[0]},${bounds.br[1]})
 `);
 
-addPoint([9, -3]);
+addPoint([3, 3]);
+
+console.log(`
+ (${bounds.tl[0]},${bounds.tl[1]}).....|.....(${bounds.tr[0]},${bounds.tr[1]})
+ ...........|.........
+ -----------|---------
+ ...........|.........
+ (${bounds.bl[0]},${bounds.bl[1]})....|.....(${bounds.br[0]},${bounds.br[1]})
+`);
+
+addPoint([-3, 8]);
+
+console.log(`
+ (${bounds.tl[0]},${bounds.tl[1]}).....|.....(${bounds.tr[0]},${bounds.tr[1]})
+ ...........|.........
+ -----------|---------
+ ...........|.........
+ (${bounds.bl[0]},${bounds.bl[1]})....|.....(${bounds.br[0]},${bounds.br[1]})
+`);
+
+addPoint([-4, -9]);
 
 console.log(`
  (${bounds.tl[0]},${bounds.tl[1]}).....|.....(${bounds.tr[0]},${bounds.tr[1]})
